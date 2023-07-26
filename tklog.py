cebe2e084424b4279500218a0e4d9f195dbb3b16
xinlin-z <xinlin.zhang@hotmail.com>
2023-07-26 21:00:32 +0800

    use deque

--- tklog.py
+++ tklog.py
@@ -4,7 +4,8 @@ from tkinter.scrolledtext import ScrolledText
 from tkinter.filedialog import asksaveasfilename
 import logging
 import threading
-import queue
+from collections import deque
+import time
 
 
 __version = 'V0.13'
@@ -19,19 +20,6 @@ thread which needs to log to GUI text windows.
 
 You can not set sync=True in the event loop of GUI!!
 """
-
-# When too many threads put info in the queue, and there is only one
-# thread to get and consume, a lot of info maybe stocked in the queue,
-# and it needs so much time to get and consume them all. So, set a length
-# to the queue to slow down all the crazy threads.
-# But, there is another risk. If your GUI event handler put info directly,
-# without in a thread, your program might be deadlock if the queue is not
-# long enough.
-# So, to be a little balance in between, here we go:
-QUEUE_LEN = 2048
-# And, put method is set with block=False.
-
-
 class tklog(ScrolledText):
     """readonly scrolled text log class"""
 
@@ -61,15 +49,13 @@ class tklog(ScrolledText):
         self.bind('<Up>', self._lineUp)
         self.bind('<Down>', self._lineDown)
         self.pList = []
-        self.q = queue.Queue(QUEUE_LEN)
+        self.q = deque()
         self.stop = 0
-        self.wt = threading.Thread(target=self._writer,
-                                   args=(), daemon=True)
-        self.wt.start()
+        threading.Thread(target=self._writer, args=(), daemon=True).start()
 
     def destroy(self):
         self.stop = 1
-        self.q.put(None)  # q.get is blocked, so we need put sth.
+        self.q.append(None)
 
     def _popup(self, event):
         self.rpop.post(event.x_root, event.y_root)
@@ -109,50 +95,48 @@ class tklog(ScrolledText):
 
     def _writer(self):
         while True:
-            info = self.q.get()
-            if self.stop: break
+            if self.stop:
+                break
             try:
-                if isinstance(info, threading.Event):
-                    info.set()
-                    continue
+                info = self.q.popleft()
+            except IndexError:
+                time.sleep(0.5)
+                continue
+            if isinstance(info, threading.Event):
+                info.set()
+                continue
+            try:
                 pos = info[:9].find('@')
+                self._chState('on')
                 if pos == -1:
-                    self._chState('on')
                     self.insert(tk.END, '[undefined format]: '+info)
-                    self._chState('off')
                 else:
                     if info[:pos] == 'CLEAR':
-                        self._chState('on')
                         self.delete('1.0', tk.END)
-                        self._chState('off')
+                        self.pList = []
                     elif info[:pos] == 'PNG' or info[:pos] == 'GIF':
                         try:
                             self.pList.append(PhotoImage(file=info[pos+1:]))
-                            self._chState('on')
                             self.image_create(tk.END, image=self.pList[-1])
                             self.insert(tk.END, '\n', 'DEBUG')
-                            self._chState('off')
                         except Exception as e:
-                            self._chState('on')
                             self.insert(tk.END, repr(e)+'\n', 'DEBUG')
-                            self._chState('off')
                     else:
-                        self._chState('on')
                         self.insert(tk.END, info[pos+1:], info[:pos])
-                        self._chState('off')
+                self._chState('off')
                 if self.autoscroll.get() == 1:
                     self.see(tk.END)
             except tk.TclError:
                 break
 
     def _log(self, level, content, end, sync):
-        self.q.put(level+'@'+content+end, block=False)
+        self.q.append(level+'@'+content+end)
         if sync:
             self._syn_log()
 
     def _syn_log(self):
         wait2go = threading.Event()
-        self.q.put(wait2go, block=False)
+        self.q.append(wait2go)
         wait2go.wait()
 
     def title(self, content, end='\n', *, sync=False):
@@ -189,7 +173,7 @@ class tklog(ScrolledText):
         self.yview('scroll', 1, 'units')
 
     def clear(self):
-        self.q.put('CLEAR@', block=False)
+        self.q.append('CLEAR@')
 
 
 class tklogHandler(logging.Handler):
